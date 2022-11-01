100b07afdf552e7264cbd95f7e1dc14bd66ed151
Philip Dimaano <pdimaano7@gmail.com>
2022-10-31 17:31:45 -0700

    Started third function and setup for card drawing

--- card-deck/script.js
+++ card-deck/script.js
@@ -35,3 +35,34 @@ async function drawTwoCards() {
 }
 
 drawTwoCards();
+
+let deckId = null;
+/** Function to request a cards from a newly shuffled deck. With each button
+ *  click, cards will be displayed until there are no cards left in the deck.
+ */
+
+async function setUp() {
+  const resp = await axios({ url: `${BASE_URL}/deck/new/shuffle/?deck_count=1` });
+  // console.log(resp.data)
+  
+}
+
+async function handleButtonClick() {
+  const firstResp = await axios({ url: `${BASE_URL}/deck/new/draw/?count=1` });
+  const deckID = firstResp.data.deck_id;
+  const secondResp = await axios({
+    url: `${BASE_URL}/deck/${deckID}/draw/?count=1`,
+  });
+  // console.log(resp.data)
+  console.log([
+    `${firstResp.data.cards[0].value.toLowerCase()} of ${firstResp.data.cards[0].suit.toLowerCase()}`,
+    `${secondResp.data.cards[0].value.toLowerCase()} of ${secondResp.data.cards[0].suit.toLowerCase()}`,
+  ]);
+}
+
+drawTwoCards();
+
+
+const $cards = $("#card-container");
+const $button = $("#card-button");
+$button.on("click", handleButtonClick());
